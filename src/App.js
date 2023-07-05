2c276c75af4e60d9160cff92254a9818f11d7cc0
Tushar Sharma <sharmatusharofficial14@gmail.com>
2023-07-05 09:01:53 +0530

    Added dark mode feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { productInputs, userInputs } from "./formSource";
+import "./style/dark.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
+import {AuthContext} from "../src/context/authContext";
 
+
+// Context for Dark mode and current user which ony allows the authorized user to use the application
 function App() {
+  const { darkMode } = useContext(DarkModeContext);
+  const {currentUser}= useContext(AuthContext);
+
+  const RequireAuth= ({children})=>{
+    return currentUser ? children : <Navigate to= "/login"/>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+          <Route path="login" element={<Login />} />
+          <Route index element={
+            <RequireAuth>
+               <Home/>
+            </RequireAuth>
+         
+          } />
+        
+            <Route path="users">
+              <Route index element={
+                  <RequireAuth>
+              <List />
+              </RequireAuth>
+              } />
+              <Route path=":userId" element={
+                 <RequireAuth>
+              <Single />
+              </RequireAuth>
+              } />
+              <Route
+                path="new"
+                element={
+                  <RequireAuth>
+                <New inputs={userInputs} title="Add New User" />
+                </RequireAuth>
+                }/>
+            </Route>
+            <Route path="products">
+              <Route index element={
+               <RequireAuth>
+              <List />
+              </RequireAuth> 
+              } />
+              <Route path=":productId" element={
+               <RequireAuth>
+               <Single />
+               </RequireAuth> } />
+              <Route
+                path="new"
+                element={
+                  <RequireAuth>
+                <New inputs={productInputs} title="Add New Product" />
+                </RequireAuth>}
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
